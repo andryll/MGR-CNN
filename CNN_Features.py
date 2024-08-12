2e54a4e59df292340d31acc2cf28d12a4c51ee79
André Luís <andryllluis@gmail.com>
2024-08-12 11:21:28 -0300

    uploading files

---
+++ CNN_Features.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 10 07:15:25 2024
+
+@author: andry
+"""
+
+# Importando as Bibliotecas
+import numpy as np
+import librosa
+import os
+import pandas as pd
+from keras.utils import to_categorical
+
+import librosa.display
+import matplotlib.pyplot as plt
+import io
+from PIL import Image
+
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+
+def splitSongs (songList, duration, sr=44100):
+
+  # Converta o tamanho da janela de segundos para amostras
+  window_size_samples = int(duration * sr)
+
+  # Inicialize uma lista para armazenar os segmentos
+  segmentedList = []
+
+  # Pega cada música da lista
+  for y in songList:
+
+    # Calcule o número total de segmentos
+    num_segments = len(y[0]) // window_size_samples
+
+    # Divida o áudio em segmentos de 5 segundos e adcione-os na lista
+    for i in range(num_segments):
+        start = i * window_size_samples
+        end = (i + 1) * window_size_samples
+        segment = (y[0][start:end], y[1], y[2])
+        segmentedList.append(segment)
+
+  # Retorna a nova lista
+  return segmentedList
+
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+
+
+def readSongs (genre, numSongs, sr=44100, duration = 30):
+
+  # Declarando listas iniciais
+  songs = []
+  genrelist = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz',
+                'metal', 'pop', 'reggae', 'rock']
+
+  # Se a escolha de gênero não for 'all', substitui a lista pelo gênero escolhido
+  if genre != 'all':
+    genrelist = [genre]
+
+  #Percorre todos os gêneros da lista
+  for g in genrelist:
+    #Pega o caminho para a pasta do gênero escolhido
+    dir_path = os.path.join('C:/Users/andry/OneDrive/Documentos/GitHub/MGR-IC/songs', g)
+    #Lista os arquivos da pasta e os embaralha
+    files = os.listdir(dir_path)
+    files.sort
+
+    # Até o número de musicas desejado ser alcançado, lê os arquivos de áudio com o librosa
+    for i in range(numSongs):
+      songs.append(librosa.load(os.path.join(dir_path, files[i]), sr=sr, mono = True, duration = 30))
+      # Adciona o gênero como uma variável da tupla
+      songs[-1] = songs[-1] + (g,)
+
+  max_len = max(len(song[0]) for song in songs)
+
+  # Garante que todas as músicas terão o mesmo tamanho da maior
+  resized_songs = []
+  for song in songs:
+      # Verifica se a música precisa ser redimensionada
+      if len(song[0]) < max_len:
+          # Adiciona zeros à direita para igualar o tamanho
+          padded_audio = librosa.util.pad_center(data = song[0], size = max_len, axis = 0)
+          resized_songs.append((padded_audio, song[1], song[2]))
+      else:
+          resized_songs.append(song)
+
+  new_songs = splitSongs (resized_songs, sr=sr, duration = duration)
+
+  return new_songs
+
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+
+def featureExtraction (songs, sr=44100):
+
+  colunas = ['spectogram', 'Classe']
+
+  df = pd.DataFrame(columns=colunas)
+
+  for y in songs:
+
+    S = librosa.feature.melspectrogram(y=y[0], sr=y[1])
+    S_dB = librosa.power_to_db(S, ref=np.max)
+
+    # Criar uma figura do matplotlib para o espectrograma
+    dpi=100
+    size = 254
+    figsize = (size/dpi, size/dpi)
+    plt.figure(figsize=figsize, dpi=dpi)
+    librosa.display.specshow(S_dB, sr=y[1])
+    plt.tight_layout()
+
+    # Salvar o espectrograma em um buffer de memória
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
+    plt.close()
+    buf.seek(0)
+
+    img = Image.open(buf).convert("RGB")
+    img_array = np.array(img)/255
+
+    #Adcionando ao DF
+
+    df.loc[len(df)] = [img_array, y[2]]
+
+  return df
+
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+
+if __name__ == "__main__":
+    #Reading all musics and splitting
+    songs = readSongs ('all', 100, 44100, 5)
+    print("Songs readed!")
+    #Getting the spectrograms
+    df = featureExtraction (songs, sr=44100)
+    #Converting the classes to numerical values
+    df['Classe'] = pd.factorize(df['Classe'])[0]
+    #Converting the classes to categorical values
+    df['Classe'] = df['Classe'].apply(lambda x: to_categorical(x, num_classes=10))
+
+    print(df['spectogram'][0].shape)
+
+    #Saving the data
+    df.to_pickle('C:/Users/andry/OneDrive/Documentos/GitHub/MGR-IC/ft2/5s/cnn_spectrograms.pkl')
+
+    print("Done!")
+
+# --------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------
---
+++ running_VGG.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 08:01:51 2024
+
+@author: andry
+"""
+
+# Importando as Bibliotecas
+
+import numpy as np
+import pandas as pd
+import random
+import keras
+
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.callbacks import EarlyStopping, CSVLogger
+from tensorflow.keras.applications import VGG16
+from sklearn.metrics import balanced_accuracy_score
+
+
+#===============================================================================
+
+def get_VGG16_model_Keras(input_shape=(224,224,3)) :
+
+	VGGmodel = Sequential()
+	baseModel = VGG16(
+		input_shape=input_shape,
+		weights='imagenet',
+		include_top=False,
+	)
+	baseModel.trainable = False
+	VGGmodel.add(baseModel)
+	VGGmodel.add(Flatten())
+	VGGmodel.add(Dense(4096,activation = 'relu'))
+	VGGmodel.add(Dense(10,activation = 'softmax'))
+
+	return(VGGmodel)
+
+
+#===============================================================================
+
+def BlockSplit(dataframe, seed, n_songs):
+
+
+  random.seed(seed)
+  n_block = round(dataframe.shape[0] / n_songs)
+  songs_per_genre = round(n_songs/10)
+  n_test = round(songs_per_genre * 0.1)
+
+  #used_list = []
+  range_list = []
+  block_lists = []
+
+  for i in range(10):
+    range_list.append(range(i*songs_per_genre, (i+1)*songs_per_genre))
+
+  for k in range(10):
+    test_list = []
+    definitive_test_list = []
+
+    for i in range(10):
+      r = random.sample(range_list[i], n_test)
+      # print(range_list[i])
+      range_list[i] = [element for element in range_list[i] if element not in r]
+
+      for j in r:
+        test_list.append(j)
+
+    test_list.sort()
+    # print(test_list)
+    for i in test_list:
+      for j in range(n_block):
+        definitive_test_list.append(round((i*n_block)+j))
+
+    # print(definitive_test_list)
+    block_lists.append(definitive_test_list)
+
+  return block_lists
+
+#===============================================================================
+
+def trainVGG16(df, n_songs):
+
+    all_performances = []
+    # all_predictions  = []
+
+    for seed in range(0, 10):#2):
+
+        print("############################")
+        print(" * Running for seed = ", seed)
+        print("############################")
+		# ----------------------------
+		# Set the seed using keras.utils.set_random_seed. This will set:
+		# 1) `numpy` seed
+		# 2) `tensorflow` random seed
+		# 3) `python` random seed
+		# ----------------------------
+        keras.utils.set_random_seed(seed)
+        test_list = BlockSplit(dataframe = df, seed = seed, n_songs=n_songs)
+
+# ----------------------------
+# Running for each fold
+# ----------------------------
+
+        temp_acc = []
+        split = 0
+
+        for test in test_list:
+
+            print("=============================")
+            print(" * Running for split = ", split)
+            print("============================")            
+
+      		# ----------------------------
+      		# Defining the prediction model (CNN)
+      		# ----------------------------
+      
+            print(" - defining VGG16 model")
+            model = get_VGG16_model_Keras(input_shape=(224,224,3))
+            model.compile(optimizer='adam', loss=CategoricalCrossentropy(), metrics=['accuracy'])
+      
+      		# ----------------------------
+      		# Traninig the algorithm
+      		# ----------------------------
+      
+      		# Callbacks
+            early_stopper = EarlyStopping(monitor="val_loss", mode="min", patience=10, verbose=1)
+            csv_logger    = CSVLogger(f"C:/Users/andry/OneDrive/Documentos/GitHub/MGR-IC/ft2/5s/results/vgg16Logs/log_history_vgg16_seed_{seed}.csv", separator=",", append=False)
+      
+            print(" - training VGG16")
+            X = np.array(df['spectogram'].iloc[~df.index.isin(test)].tolist())
+            y = np.array(df['Classe'].iloc[~df.index.isin(test)].tolist())
+            X_test = np.array(df['spectogram'].iloc[df.index.isin(test)].tolist())
+            y_test = np.array(df['Classe'].iloc[df.index.isin(test)].tolist())
+            # print(X.shape)
+            # print(y.shape)
+      
+            # history  = model.fit(X, y, epochs=20, validation_split=0.1, batch_size=32, callbacks=[early_stopper, csv_logger])
+            model.fit(X, y, epochs=20, validation_split=0.1, batch_size=64, callbacks=[early_stopper, csv_logger])
+
+      		# ----------------------------
+      		# Evaluating predictions
+      		# ----------------------------
+            print(" - Evaluating VGG16")
+            predictions = model.predict(X_test)
+            rounded_predictions = np.argmax(predictions, axis=1)
+      
+      		# ----------------------------
+      		# adding predictions to a data frame
+      		# ----------------------------
+      		# preds   = pd.DataFrame(rounded_predictions, index = df_testing.index)
+      		# preds   = preds.rename(columns={0: 'predictions'})
+      		# df_pred = pd.concat([df_testing, preds], axis = 1) # by column
+      
+      		# ----------------------------
+      		# evaluating with scikit learn metrics
+      		# ----------------------------
+      		# acc = accuracy_score(test_labels, rounded_predictions)
+            bac = balanced_accuracy_score(np.argmax(y_test, axis=1), rounded_predictions)
+      		# f1s = f1_score(test_labels, rounded_predictions)
+      		# print("acc = ", acc)
+            print("bac = ", bac)
+      		# print("f1c = ", f1s)
+            temp_acc.append(bac)
+            split += 1
+            
+        print("----------------------------")
+        all_performances.append([np.mean(temp_acc), seed])
+		# all_predictions.append(df_pred)
+
+	# ---------------------------------------------------------
+	# Binding all predictions and performances
+	# ---------------------------------------------------------
+	# pred_results = pd.concat(all_predictions, axis = 0) # by row
+	# pred_results[['algo']] = "VGG16"
+
+    perf_results = pd.DataFrame(all_performances, columns=["bac", "seed"])
+    return perf_results
+
+
+#================================================================================
+
+if __name__ == "__main__":
+
+    df = pd.read_pickle('C:/Users/andry/OneDrive/Documentos/GitHub/MGR-IC/ft2/5s/cnn_spectrograms.pkl')
+
+    perf_results = trainVGG16(df=df, n_songs = 100)
+    	
+    perf_results.to_csv('C:/Users/andry/OneDrive/Documentos/GitHub/MGR-IC/ft2/5s/performances_vgg16.csv', index = False)
+    print("Done!")
+
+
